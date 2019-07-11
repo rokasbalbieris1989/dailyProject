1b0802f40f44ede37adc0d54d4f2011edb27b107
rokasbalbieris1989 <51324248+rokasbalbieris1989@users.noreply.github.com>
2019-07-11 13:17:56 +0300

    Add files via upload

---
+++ EshopMusicInstruments/EshopMusicInstruments/src/main/java/com/mycompany/eshopmusicinstruments/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.mycompany.eshopmusicinstruments.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
